97c579c1796ca472173c3be9c63ee0a260d0b10e
Mike Austin <mike_ekim@yahoo.com>
2023-06-23 09:07:52 -0700

    Tweak shadow

--- bare/src/components/view/ViewStyles.ts
+++ bare/src/components/view/ViewStyles.ts
@@ -34,7 +34,7 @@ const useStyles = createUseStyles({
       content: '""',
       position: 'absolute',
       inset: 0,
-      boxShadow: '0 2px 4px hsla(0, 0%, 0%, 0.1)',
+      boxShadow: '0 2px 2px hsla(0, 0%, 0%, 0.1)',
       borderRadius: 2.5,
       pointerEvents: 'none',
     }
