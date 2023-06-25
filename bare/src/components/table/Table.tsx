c7b929b842395a502e2465841e03331bc583fe76
Mike Austin <mike_ekim@yahoo.com>
2023-06-25 10:14:38 -0700

    Tweak Table

--- bare/src/components/table/Table.tsx
+++ bare/src/components/table/Table.tsx
@@ -1,5 +1,8 @@
+import { createUseStyles } from 'react-jss';
+
 import View, { ViewProps } from "../view/index.js";
 import Text from "../text/index.js";
+import clsx from 'clsx';
 
 type FieldDefinition<T> = {
   key: keyof T,
@@ -10,17 +13,47 @@ type FieldDefinition<T> = {
 type Record = {
   name: string,
   scienceName: string,
+  kingdom: string,
+  order: string,
 };
 
 const columns: FieldDefinition<Record>[] = [
   { key: 'name', label: 'Animal' },
   { key: 'scienceName', label: 'Scientific Name' },
+  { key: 'kingdom', label: 'Kingdom' },
+  { key: 'order', label: 'Order' }
 ];
 
 const rows: Record[] = [
-  { name: 'Tiger', scienceName: 'Panthera Tigris' },
-  { name: 'Wolf', scienceName: 'Canis lupus' },
+  { name: 'Tiger', scienceName: 'Panthera Tigris', kingdom: 'Animalia', order: 'Carnivora' },
+  { name: 'Wolf', scienceName: 'Canis lupus', kingdom: 'Animalia', order: 'Carnivora' },
+  { name: 'Monkey', scienceName: 'Cercopithecidae', kingdom: 'Animalia', order: 'Primates' },
 ];
+
+
+const useStyles = createUseStyles({
+  Table: {
+
+  },
+  Row: {
+  },
+  Cell: {
+  },
+  Header: {
+    padding: '20px 0 8px 16px',
+  },
+  Text: {
+    padding: '12px 0 12px 16px',
+  },
+  borderless: {
+    '& $Cell:first-child $Header, & $Cell:first-child $Text': {
+      paddingLeft: 0,
+    },
+    '& $Cell $Header': {
+      paddingTop: 0,
+    },
+  }
+});
 
 type TableProps = {
   borderless?: boolean,
@@ -30,20 +63,27 @@ const Table = ({
   borderless,
   ...props
 }: TableProps) => {
+  const styles = useStyles();
+
+  const tableClassName = clsx(
+    styles.Table,
+    borderless && styles.borderless,
+  );
+
   return (
-    <View as="table" border={!borderless} style={{ display: 'table', borderSpacing: 0 }} {...props}>
+    <View as="table" border={!borderless} className={tableClassName} style={{ display: 'table', borderCollapse: 'collapse', borderSpacing: 0 }} {...props}>
       <thead>
         <tr>
           {columns.map((column, index) => (
-            <th key={column.key} style={{ padding: 0 }}>
+            <th key={column.key} className={styles.Cell} style={{ padding: 0, borderBottom: '1px solid hsla(0, 0%, 0%, 0.15)' }}>
               <Text
                 caps
                 fontSize="xxsmall"
                 fontWeight="semibold"
                 textColor="gray-6"
                 textAlign="left"
-                padding={borderless ? 'medium none' : 'medium large'}
-                fillColor={!borderless ? "gray-2" : undefined}
+                fillColor={!borderless ? "gray-1" : undefined}
+                className={styles.Header}
               >
                 {column.label}
               </Text>
@@ -55,8 +95,8 @@ const Table = ({
         {rows.map((row, index) => (
           <tr>
             {columns.map((column) => (
-              <td style={{ borderBottom: borderless || index !== rows.length - 1 ? '1px solid lightgray' : undefined }}>
-                <Text padding={borderless ? 'medium none' : 'medium large'}>{row[column.key]}</Text>
+              <td className={styles.Cell} style={{ borderBottom: borderless || index !== rows.length - 1 ? '1px solid hsla(0, 0%, 0%, 0.15)' : undefined }}>
+                <Text className={styles.Text}>{row[column.key]}</Text>
               </td>
             ))}
           </tr>
