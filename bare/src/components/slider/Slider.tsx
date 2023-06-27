eb1f7dbc9bc3a2fe67d99415d91fb4a6db46885b
Mike Austin <mike_ekim@yahoo.com>
2023-06-26 19:49:17 -0700

    Style slider

--- bare/src/components/slider/Slider.tsx
+++ bare/src/components/slider/Slider.tsx
@@ -6,18 +6,23 @@ import View from '../view/index.js';
 const useStyles = createUseStyles({
   Inner: {
     appearance: 'none',
-    background: 'none',
+    background: OpenColor.gray[3],
+    borderRadius: 1000,
     padding: 0,
     margin: 0,
-    '&::-webkit-slider-runnable-track': {
-      background: OpenColor.gray[3],
-      borderRadius: 1000,
-      height: 8,
-    },
+    height: 8,
+    // '&::-webkit-slider-runnable-track': {
+    //   background: OpenColor.gray[3],
+    //   borderRadius: 1000,
+    //   height: 8,
+    // },
     '&::-webkit-slider-thumb': {
       appearance: 'none',
       background: 'white',
-      height: 8,
+      width: 24,
+      height: 24,
+      borderRadius: 1000,
+      boxShadow: 'inset 0 0 0 1px hsla(0, 0%, 0%, 0.15)',
     }
   }
 });
