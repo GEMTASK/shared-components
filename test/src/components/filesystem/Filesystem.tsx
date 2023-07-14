4851d9056e2afab26e9d7e7b3f9edabd0d460e4c
Mike Austin <mike_ekim@yahoo.com>
2023-07-14 11:04:31 -0700

    Add multiple views to Files

--- test/src/components/filesystem/Filesystem.tsx
+++ test/src/components/filesystem/Filesystem.tsx
@@ -1,54 +1,88 @@
 import { useState } from 'react';
 import { LoremIpsum } from 'lorem-ipsum';
 
-import { Button, Divider, Grid, Spacer, Splitter, Text, View } from 'bare';
+import { Button, Divider, Grid, Spacer, Splitter, Table, Text, View } from 'bare';
 
 const lorem = new LoremIpsum();
 
-const files = Array.from({ length: 30 }, (_, index) => ({
-  filename: lorem.generateWords(4),
-}));
+const files = Array.from({ length: 30 }, () => ({
+  filename: lorem.generateWords(4).split(' ').map(([a, ...rest]) => a.toUpperCase() + rest.join('')).join(' ') + '.png',
+  size: Math.random() * 100,
+})).sort((a, b) => a.filename < b.filename ? -1 : 1);
 
-enum ViewType {
-  Icon,
-  List,
-  Table,
-}
+// enum ViewType {
+//   Icon,
+//   List,
+//   Table,
+// }
 
 const IconView = () => {
   return (
-    <View padding="large" style={{ overflow: 'auto' }}>
-      <Grid align="top left" gap={20} style={{ gridTemplateColumns: 'repeat(auto-fit, minmax(100px, 1fr))' }}>
-        {files.map(({ filename }) => (
-          <View align="top">
-            <View fillColor="gray-1" style={{ width: 48, height: 48 }} />
-            <Spacer size="small" />
-            <Text lineClamp={2} textAlign="center">{filename}</Text>
-          </View>
-        ))}
-      </Grid>
-    </View>
+    <Grid align="top left" gap={20} style={{ gridTemplateColumns: 'repeat(auto-fit, minmax(150px, 1fr))' }}>
+      {files.map(({ filename }) => (
+        <View align="top">
+          <View fillColor="gray-3" style={{ width: 48, height: 48, borderRadius: 2.5 }} />
+          <Spacer size="small" />
+          <Text lineClamp={2} textAlign="center">{filename}</Text>
+        </View>
+      ))}
+    </Grid>
   );
 };
 
 const ListView = () => {
   return (
-    <View>
-
-    </View>
+    <Grid align="top left" gap={16} style={{ gridTemplateColumns: 'repeat(auto-fit, minmax(200px, 1fr))' }}>
+      {files.map(({ filename }) => (
+        <View horizontal align="left">
+          <View fillColor="gray-3" style={{ width: 24, height: 24, borderRadius: 2.5, flexShrink: 0 }} />
+          <Spacer size="small" />
+          <Text lineClamp={1}>{filename}</Text>
+        </View>
+      ))}
+    </Grid>
   );
 };
 
+const DetailsView = () => {
+  return (
+    <Grid align="top left" gap={16} style={{ gridTemplateColumns: 'repeat(auto-fit, minmax(200px, 1fr))' }}>
+      {files.map(({ filename, size }) => (
+        <View horizontal align="left">
+          <View fillColor="gray-3" style={{ width: 40, height: 40, borderRadius: 2.5, flexShrink: 0 }} />
+          <Spacer size="small" />
+          <View>
+            <Text lineClamp={1}>{filename}</Text>
+            <Spacer size="small" />
+            <Text fontSize="xsmall" textColor="gray-6" lineClamp={1}>{size.toFixed(2)} KiB</Text>
+          </View>
+        </View>
+      ))}
+    </Grid>
+  );
+};
+
+const TableView = () => {
+  return (
+    <Table />
+  );
+};
+
+type ViewType = typeof IconView | typeof ListView | typeof DetailsView;
+
 const Filesystem = () => {
-  const [selectedViewType, setViewType] = useState<ViewType>(ViewType.Icon);
+  const [selectedView, setView] = useState<ViewType>(() => IconView);
 
   const ToolbarButton = ({ viewType, ...props }: any) => {
-    const handleClick = () => setViewType(viewType);
+    const handleClick = () => setView(() => viewType);
 
+    console.log(selectedView === viewType);
     return (
-      <Button hover selected={selectedViewType === viewType} {...props} onClick={handleClick} />
+      <Button hover selected={selectedView === viewType} {...props} onClick={handleClick} />
     );
   };
+
+  const Component = selectedView;
 
   return (
     <>
@@ -58,12 +92,15 @@ const Filesystem = () => {
         </View>
         <View flex>
           <View horizontal padding="small" fillColor="gray-1">
-            <ToolbarButton icon="square" viewType={ViewType.Icon} />
-            <ToolbarButton icon="list" viewType={ViewType.List} />
-            <ToolbarButton icon="border-all" viewType={ViewType.Table} />
+            <ToolbarButton icon="square" viewType={IconView} />
+            <ToolbarButton icon="table-list" viewType={DetailsView} />
+            <ToolbarButton icon="list" viewType={ListView} />
+            <ToolbarButton icon="border-all" viewType={TableView} />
           </View>
           <Divider />
-          <IconView />
+          <View padding="large" style={{ overflow: 'auto' }}>
+            <Component />
+          </View>
         </View>
       </Splitter>
     </>
