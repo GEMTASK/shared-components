108c7e048f8c4169170c02ddcfe2716b110af6d3
Mike Austin <mike_ekim@yahoo.com>
2023-07-28 13:14:35 -0700

    implement makeIterable

--- test/src/components/terminal/kopi-language/src/classes/KopiStream.ts
+++ test/src/components/terminal/kopi-language/src/classes/KopiStream.ts
@@ -3,45 +3,56 @@ import { Context, KopiValue } from '../types';
 import KopiNumber from './KopiNumber';
 import KopiFunction from './KopiFunction';
 import KopiTuple from './KopiTuple';
-import KopiArray from './KopiArray';
 
 interface Iterable<T extends KopiValue> {
   [Symbol.asyncIterator](): AsyncIterator<KopiValue>;
 
-  map(func: KopiFunction, context: Context): KopiStream<T>;
-  filter(func: KopiFunction, context: Context): KopiStream<T>;
+  // map(func: KopiFunction, context: Context): KopiStream<T>;
+  // filter(func: KopiFunction, context: Context): KopiStream<T>;
   take(count: KopiNumber): KopiStream<T>;
 }
 
-// function make<A extends Iterable<T>, T extends KopiValue>(from?: (iterable: AsyncIterable<KopiValue>) => Promise<T>) {
-//   return class XIterable {
-//     take(count: KopiNumber) {
-//       let index = 0;
+function makeIterable<TIterable extends Iterable<TKopiValue>, TKopiValue extends KopiValue>(
+  from?: (iterable: AsyncIterable<KopiValue>) => Promise<TKopiValue>
+) {
+  abstract class Mixin implements Iterable<TKopiValue> {
+    abstract [Symbol.asyncIterator](): AsyncIterator<KopiValue>;
 
-//       const generator = async function* (this: A) {
-//         for await (const value of this) {
-//           if (++index <= count.value) {
-//             yield value;
-//           } else {
-//             break;
-//           }
-//         }
-//       }.apply(this);
+    take(this: TIterable, count: KopiNumber) {
+      let index = 0;
 
-//       return new KopiStream(generator, from); // TODO
-//     }
-//   }
-// }
+      const generator = async function* (this: TIterable) {
+        for await (const value of this) {
+          if (++index <= count.value) {
+            yield value;
+          } else {
+            break;
+          }
+        }
+      }.apply(this);
+
+      return new KopiStream(generator, from);
+    }
+  }
+
+  return Mixin;
+}
 
 class KopiStream<T extends KopiValue = any> extends KopiValue implements Iterable<T> {
   readonly iterable: AsyncIterable<KopiValue>;
   readonly from?: (iterable: AsyncIterable<KopiValue>) => Promise<T>;
+
+  take: (count: KopiNumber) => KopiStream<T>;
 
   constructor(iterable: AsyncIterable<KopiValue>, from?: (iterable: AsyncIterable<KopiValue>) => Promise<T>) {
     super();
 
     this.iterable = iterable;
     this.from = from;
+
+    const cls = makeIterable(this.from);
+
+    this.take = cls.prototype.take;
   }
 
   // TODO: Reproduce React.ReactElement type
@@ -93,21 +104,21 @@ class KopiStream<T extends KopiValue = any> extends KopiValue implements Iterabl
     return new KopiStream(generator, this.from);
   }
 
-  take(count: KopiNumber) {
-    let index = 0;
+  // take(count: KopiNumber) {
+  //   let index = 0;
 
-    const generator = async function* (this: KopiStream<T>) {
-      for await (const value of this) {
-        if (++index <= count.value) {
-          yield value;
-        } else {
-          break;
-        }
-      }
-    }.apply(this);
+  //   const generator = async function* (this: KopiStream<T>) {
+  //     for await (const value of this) {
+  //       if (++index <= count.value) {
+  //         yield value;
+  //       } else {
+  //         break;
+  //       }
+  //     }
+  //   }.apply(this);
 
-    return new KopiStream(generator, this.from); // TODO
-  }
+  //   return new KopiStream(generator, this.from); // TODO
+  // }
 }
 
 export default KopiStream;
