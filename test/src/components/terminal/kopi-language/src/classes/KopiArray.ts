e88fa646179447ad2d27599a770adc3031951960
Mike Austin <mike_ekim@yahoo.com>
2023-07-31 09:55:26 -0700

    Add Iterable methods to KopiArray

--- test/src/components/terminal/kopi-language/src/classes/KopiArray.ts
+++ test/src/components/terminal/kopi-language/src/classes/KopiArray.ts
@@ -1,6 +1,30 @@
 import { KopiValue } from '../types';
 
 import KopiNumber from './KopiNumber';
+import type { KopiStream } from './KopiStream';
+import type { KopiIterable } from './KopiIterable';
+
+let ArrayStream: {
+  new(iterable: AsyncIterable<KopiValue>): KopiStream<KopiArray>;
+};
+
+let ArrayIterable: {
+  new(Stream: {
+    new(iterable: AsyncIterable<KopiValue>): KopiIterable<KopiArray>;
+  }): KopiIterable<KopiArray>;
+};
+
+import('./KopiStream').then((result) => {
+  ArrayStream = result.default(KopiArray.fromIterable);
+
+  import('./KopiIterable').then((result) => {
+    ArrayIterable = result.default(ArrayStream);
+
+    KopiArray.prototype.map = ArrayIterable.prototype.map;
+    KopiArray.prototype.filter = ArrayIterable.prototype.filter;
+    KopiArray.prototype.take = ArrayIterable.prototype.take;
+  });
+});
 
 async function fromIterable(iterable: AsyncIterable<KopiValue>) {
   let elements: KopiValue[] = [];
@@ -11,6 +35,8 @@ async function fromIterable(iterable: AsyncIterable<KopiValue>) {
 
   return new KopiArray(elements);
 }
+
+interface KopiArray extends KopiValue, KopiIterable<KopiArray> { };
 
 class KopiArray extends KopiValue {
   // static readonly emptyValue = () => new KopiArray([]);
@@ -37,6 +63,12 @@ class KopiArray extends KopiValue {
     );
 
     return `[${elements.join(', ')}]`;
+  }
+
+  async *[Symbol.asyncIterator]() {
+    for (const value of this.elements) {
+      yield value;
+    }
   }
 
   size() {
