cadd9536946c716bdec3f9b66808879138ad5423
Mike Austin <mike_ekim@yahoo.com>
2023-08-10 09:19:46 -0700

    Move examples and functions to their own files

---
+++ test/src/components/terminal/examples.ts
@@ -0,0 +1,120 @@
+const examples = [
+  `date`,
+  `clock`,
+  `calendar`,
+  `1 + 2 * 3`,
+  `(1 + 2) * 3`,
+  `random 4.5..5.5`,
+  `sleep 5 + sleep 5`,
+  `("a", "b", "c").1`,
+  `((), 1, 'ast, "3", [])`,
+  `(a => b => a + b) 1 2`,
+  `((a, b) => a + b) (1, 2)`,
+  `((a, b = 2) => a + b) 1`,
+  // `1..3 | flatMap a => ((a + 1)..3 | map b => (a, b))`,
+  `((a = 1, b = 2) => a + b) ()`,
+  `let (a = 1) => a`,
+  `let (a = 1, b = 2) => a + b`,
+  `('1, 'sin, '(sin 30))`,
+  `'sin (30 * (PI / 180))`,
+  `'cos (30 * (PI / 180))`,
+  `fetch "robots.txt"`,
+  `'size (fetch "robots.txt")`,
+  `
+add1 = n => n + 1
+add1 5
+`,
+  `"abc" | map (c) => 'toUpper c`,
+  `"🥥🍏🍓" | map 'succ`,
+  `"🥥🍏🍓" | at 1`,
+  `"abc" | split | map 'succ`,
+  `"a,b,c" | split ","`,
+  `("ab", "xy") | map (a, x) => a x`,
+  `[(1..5).from, (1..5).to]`,
+  `1..5 | take 3 | map (n) => n * n`,
+  `1..(fetch "robots.txt" | size)`,
+  `1..5 | take 3 | map '(toFixed 2)`,
+  `1..10 | filter (n) => n % 2 == 0`,
+  `", " | combine 1..3`,
+  `1..3 | join ", "`,
+  `", " | combine "abc"`,
+  `"abc" | join ", "`,
+  `1..3 | map '(toFixed 2) | join ", "`,
+  `1..5 | reduce (a = 1, n) => a * n`,
+  `"abc" | reduce (a, c) => a ", " c`,
+  `[sleep 5, sleep 5]`,
+  `[1, "two", 1..3, n => n * n]`,
+  `[1, 2, 3] | at 1`,
+  `[1, 2] | reduce (a, n) => a + n`,
+  `[1, 2, 3] | take 2 | take 1`,
+  `(1..2, 3..4) | map (a, b) => a * b`,
+  `1..3 | repeat | take 7`,
+  `0..0.5 (0.1) | map '(toFixed 1)`,
+  `(1 == 1, "a" == "a", 'b == 'b)`,
+  `(1, "a", 'b) == (1, "a", 'b)`,
+  `[1, "a", 'b] == [1, "a", 'b]`,
+  `[1, 2, 3] == 'toArray 1..3`,
+  `
+fns = ['(map n => n * n)]
+fns | reduce (x = 1..5, f) => f x
+`,
+  `1..3 | combos`,
+  `
+1..4 | combos | map (a, b) => {
+  a * b
+}
+`,
+  `"abc" | combos`,
+  `[1, 2, 3] | combos`,
+  `1..3 | some n => n == 2`,
+  `"abc" | some c => c == "b"`,
+  `['a, 'b, 'c] | some c => c == 'b`,
+  `"aaa" | every c => c == "a"`,
+  `['a, 'a, 'a] | every c => c == 'a`,
+  `1..3 | find (n) => n == 2`,
+  `[1, 2, 3] | find (n) => n == 2`,
+  `"abc" | find (c) => c == "b"`,
+  `1..5 | count (n) => 'even n`,
+  `[1, 2, 3] | count (n) => 'even n`,
+  `"abc" | count (c) => c == "b"`,
+  `1..5 | map '(* 3) | count 'even`,
+  `1..3 | includes 2`,
+  `"abc" | includes "b"`,
+  `[1, 2, 3] | includes 2`,
+  `1..10 | splitOn 3`,
+  `"aa,b,cc" | splitOn ","`,
+  `[1, 1, 0, 2, 0, 3, 3] | splitOn 0`,
+  `1..10 | splitEvery 3`,
+  `"abcdefghij" | splitEvery 3`,
+  `[1, 2, 3] | splitEvery 2`,
+  `
+1..15 | map (n) => {
+  match (n % 3, n % 5) (
+    (0, 0) => "FizzBuzz"
+    (0, _) => "Fizz"
+    (_, 0) => "Buzz"
+    _      => n
+  )
+}
+`,
+  `
+fact = n => let (n = n, a = 1) => {
+  match n (
+    0 => a
+    n => loop (n - 1, a * n)
+  )
+}
+[fact 5, fact 6, fact 7]
+`,
+  `
+coro = spawn () => {
+  let (x = 1) => {
+    yield n => n + x
+    loop (x + 1)
+  }
+}
+(coro | send 5, coro | send 5)
+`,
+];
+
+export default examples;
---
+++ test/src/components/terminal/functions.tsx
@@ -0,0 +1,255 @@
+import { KopiArray, KopiFunction, KopiNumber, KopiString, KopiTuple } from './kopi-language/src/classes';
+import { Context, KopiValue } from './kopi-language/src/types';
+import KopiStream_T from './kopi-language/src/classes/KopiStream';
+import KopiRange from './kopi-language/src/classes/KopiRange';
+
+import { Icon } from 'bare';
+import Clock from '../clock/Clock';
+import Calendar from '../calendar/Calendar';
+
+class KopiDate extends KopiValue implements KopiValue {
+  override async inspect() {
+    return new Date().toLocaleString();
+  }
+
+  async apply(thisArg: this, [value]: [value: KopiValue]) {
+    return value;
+  }
+}
+
+class KopiSleep extends KopiValue {
+  async apply(thisArg: this, [seconds]: [KopiNumber]) {
+    return new Promise((resolve) => {
+      setTimeout(() => resolve(seconds), seconds.value * 1000);
+    });
+  }
+}
+
+class KopiMatch extends KopiValue {
+  async apply(thisArg: this, [value, context]: [KopiValue, Context]) {
+    return async (funcs: KopiTuple) => {
+      for await (const func of funcs.fields) {
+        if (await (func as KopiFunction).parameterPattern.test(value, context)) {
+          return (func as KopiFunction).apply(KopiTuple.empty, [value, context]);
+        }
+      }
+
+      throw new Error('Match failed');
+    };
+  }
+}
+
+class KopiLet extends KopiValue {
+  async apply(thisArg: this, [func, context]: [KopiFunction, Context]) {
+    let result: KopiValue = KopiTuple.empty;
+
+    do {
+      result = result instanceof KopiLoop ? result.value : result;
+
+      result = await func.apply(KopiTuple.empty, [result, context]);
+    } while (result instanceof KopiLoop);
+
+    return result instanceof KopiLoop ? result.value : result;
+  }
+}
+
+class KopiLoop extends KopiValue {
+  constructor(value: KopiValue) {
+    super();
+
+    this.value = value;
+  }
+
+  value: KopiValue;
+}
+
+class KopiLoopFunction extends KopiValue {
+  async apply(thisArg: this, [value, context]: [KopiValue, Context]) {
+    return new KopiLoop(value);
+  }
+}
+
+class KopiApply extends KopiValue {
+  async apply(thisArg: this, [func, context]: [KopiFunction, Context]) {
+    return (arg: KopiValue) => {
+      return func.apply(KopiTuple.empty, [arg, context]);
+    };
+  }
+}
+
+class KopiIdent extends KopiValue {
+  async apply(thisArg: this, [value, context]: [KopiFunction, Context]) {
+    return value;
+  }
+}
+
+class KopiFetch extends KopiValue {
+  async apply(thisArg: this, [url, context]: [KopiString, Context]) {
+    return new KopiString((await (await fetch(url.value)).text()));
+  }
+}
+
+class KopiClock extends KopiValue {
+  async inspect() {
+    return <Clock style={{ width: 150 }} />;;
+  }
+}
+
+class KopiCalendar extends KopiValue {
+  async inspect() {
+    return <Calendar style={{ width: 360 }} />;;
+  }
+}
+
+class KopiIcon extends KopiValue {
+  async inspect() {
+    return <Icon icon="house" size="3x" />;
+  }
+}
+
+class KopiRandom extends KopiValue {
+  async apply(thisArg: this, [range, context]: [KopiRange, Context]) {
+    const from = (await range.from as KopiNumber).value;
+    const to = (await range.to as KopiNumber).value;
+
+    return new KopiNumber(Math.random() * (to - from) + from);
+  }
+}
+
+class KopiRepeat extends KopiValue {
+  static RepeatStream = KopiStream_T(KopiArray.fromIterable);
+
+  async apply(thisArg: this, [func, context]: [KopiFunction, Context]) {
+    const generator = async function* (this: KopiValue) {
+      for (let n = 0; ; ++n) {
+        if (n >= 10) break;
+
+        yield new KopiNumber(n);
+      }
+    }.apply(KopiTuple.empty, []);
+
+    return new KopiRepeat.RepeatStream(generator);
+  }
+}
+
+class KopiMeter extends KopiValue {
+  async apply(thisArg: this, [value]: [KopiNumber]) {
+    return new MetricUnit(value);
+  }
+}
+
+class MetricUnit extends KopiValue {
+  value: KopiNumber;
+
+  constructor(value: KopiNumber) {
+    super();
+
+    this.value = new KopiNumber(value.value * 1000);
+  }
+
+  // toImperial() {
+  //   return new ImperialUnit(new KopiNumber(this.value.value * 3.28084));
+  // }
+}
+
+function sleep() {
+  return new Promise((resolve) =>
+    setTimeout(resolve, 2000));
+}
+
+class Deferred<T> {
+  resolve?: (value: T) => void;
+  reject?: (reason?: any) => void;
+
+  promise: Promise<T>;
+
+  constructor() {
+    this.promise = new Promise<T>((resolve, reject) => {
+      this.resolve = resolve;
+      this.reject = reject;
+    });
+  }
+}
+
+class Channel {
+  promiseQueue: Deferred<[KopiValue, Deferred<KopiValue>]>[] = [];
+  valueQueue: [KopiValue, Deferred<KopiValue>][] = [];
+
+  read() {
+    const envelope = new Deferred<[KopiValue, Deferred<KopiValue>]>();
+
+    if (this.valueQueue.length > 0) {
+      const value = this.valueQueue.shift();
+
+      if (value) {
+        return Promise.resolve(value);
+      }
+    } else {
+      this.promiseQueue.push(envelope);
+    }
+
+    return envelope.promise;
+  }
+
+  write(value: KopiValue) {
+    const reply = new Deferred<KopiValue>();
+
+    if (this.promiseQueue.length > 0) {
+      const envelope = this.promiseQueue.shift();
+
+      envelope?.resolve?.([value, reply]);
+    } else {
+      this.valueQueue.push([value, reply]);
+    }
+
+    return reply.promise;
+  }
+}
+
+//
+
+class Coroutine extends KopiValue {
+  channel: Channel = new Channel();
+
+  async yield(func: KopiFunction, context: Context) {
+    const [value, reply] = await this.channel.read();
+
+    const result = await func.apply(KopiTuple.empty, [value, context]);
+
+    reply.resolve?.(result);
+  }
+
+  send(value: KopiValue) {
+    return this.channel.write(value);
+  }
+}
+
+class KopiSpawn extends KopiValue {
+  async apply(thisArg: this, [func, context]: [KopiFunction, Context]) {
+    const coro = new Coroutine();
+
+    (func.environment as any).yield = coro.yield.bind(coro);
+
+    func.apply(KopiTuple.empty, [KopiTuple.empty, context]);
+
+    return coro;
+  }
+}
+
+export {
+  KopiDate,
+  KopiSleep,
+  KopiMatch,
+  KopiLet,
+  KopiLoopFunction,
+  KopiApply,
+  KopiIdent,
+  KopiFetch,
+  KopiClock,
+  KopiCalendar,
+  KopiIcon,
+  KopiRandom,
+  KopiRepeat,
+  KopiMeter,
+  KopiSpawn,
+};
