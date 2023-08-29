ff5c2d513b977d541f0b0e31828a184e61d3f779
Mike Austin <mike_ekim@yahoo.com>
2023-08-29 15:24:48 -0700

    Add Dict iterable methods

--- test/src/components/terminal/kopi-language/src/classes/KopiDict.ts
+++ test/src/components/terminal/kopi-language/src/classes/KopiDict.ts
@@ -1,20 +1,66 @@
 import { Context, KopiValue } from '../types';
 
 import KopiNumber from './KopiNumber';
-import KopiBoolean from './KopiBoolean';
 import KopiTuple from './KopiTuple';
-import KopiRange from './KopiRange';
-import KopiFunction from './KopiFunction';
 import KopiString from './KopiString';
 import KopiArray from './KopiArray';
 
 import type { KopiStream } from './KopiStream';
 import type { KopiIterable } from './KopiIterable';
 
-// Dict [(1, "One"), (2, "Two")]
-// {1: "One"} << {2: "Two"}
+interface KopiDict extends KopiValue, KopiIterable<KopiValue> { };
 
-async function fromIterable(iterable: AsyncIterable<KopiTuple>) {
+let DictStream: {
+  new(iterable: AsyncIterable<KopiValue>): KopiStream<KopiDict>;
+};
+
+let ArrayStream: {
+  new(iterable: AsyncIterable<KopiValue>): KopiStream<KopiArray>;
+};
+
+let DictIterable: {
+  new(Stream: {
+    new(iterable: AsyncIterable<KopiValue>): KopiIterable<KopiDict>;
+  }): KopiIterable<KopiDict>;
+};
+
+let ArrayIterable: {
+  new(Stream: {
+    new(iterable: AsyncIterable<KopiValue>): KopiIterable<KopiArray>;
+  }): KopiIterable<KopiArray>;
+};
+
+import('./KopiStream').then((result) => {
+  DictStream = result.KopiStream_T(fromIterable);
+  ArrayStream = result.KopiStream_T(KopiArray.fromIterable);
+
+  import('./KopiIterable').then((result) => {
+    DictIterable = result.KopiIterable_T(DictStream, fromIterable);
+    ArrayIterable = result.KopiIterable_T(ArrayStream, KopiArray.fromIterable);
+
+    KopiString.prototype.toArray = ArrayIterable.prototype.toArray;
+    KopiDict.prototype.map = ArrayIterable.prototype.map;
+    KopiDict.prototype.flatMap = ArrayIterable.prototype.flatMap;
+    KopiDict.prototype.filter = DictIterable.prototype.filter;
+    KopiDict.prototype.reduce = DictIterable.prototype.reduce;
+    KopiDict.prototype.take = DictIterable.prototype.take;
+    KopiDict.prototype.skip = DictIterable.prototype.skip;
+    KopiDict.prototype.repeat = DictIterable.prototype.repeat;
+    KopiDict.prototype.join = DictIterable.prototype.join;
+    KopiDict.prototype.count = DictIterable.prototype.count;
+    KopiDict.prototype.splitOn = DictIterable.prototype.splitOn;
+    KopiDict.prototype.splitAt = DictIterable.prototype.splitAt;
+    KopiDict.prototype.splitEvery = DictIterable.prototype.splitEvery;
+
+    KopiDict.prototype.combos = DictIterable.prototype.combos;
+    KopiDict.prototype.some = DictIterable.prototype.some;
+    KopiDict.prototype.every = DictIterable.prototype.every;
+    KopiDict.prototype.find = DictIterable.prototype.find;
+    KopiDict.prototype.includes = DictIterable.prototype.includes;
+  });
+});
+
+async function fromIterable(iterable: AsyncIterable<KopiValue>) {
   let entries: [string, [KopiValue, KopiValue]][] = [];
 
   for await (const entry of iterable) {
@@ -27,17 +73,21 @@ async function fromIterable(iterable: AsyncIterable<KopiTuple>) {
   return new KopiDict(entries);
 }
 
+//
+//
+//
+
 class KopiDict extends KopiValue implements AsyncIterable<KopiValue> {
   static async fromIterable(iterable: AsyncIterable<KopiTuple>) {
     return fromIterable(iterable);
   }
 
-  map: Map<string, [KopiValue, KopiValue | Promise<KopiValue>]>;
+  _map: Map<string, [KopiValue, KopiValue | Promise<KopiValue>]>;
 
   constructor(entries: [string, [KopiValue, (KopiValue | Promise<KopiValue>)]][]) {
     super();
 
-    this.map = new Map(entries.map(([key, value]) => [
+    this._map = new Map(entries.map(([key, value]) => [
       key,
       value
     ]));
@@ -50,21 +100,25 @@ class KopiDict extends KopiValue implements AsyncIterable<KopiValue> {
   override async inspect() {
     const entries: string[] = [];
 
-    for (const [_, [key, value]] of this.map) {
+    for (const [_, [key, value]] of this._map) {
       entries.push(`${await key.inspect()}: ${await (await value).inspect()}`);
     }
 
     return `{ ${entries.join(', ')} }`;
   }
 
-  async *[Symbol.asyncIterator]() {
-    for (const [_, [key, value]] of this.map) {
+  async *[Symbol.asyncIterator](): AsyncIterator<KopiValue> {
+    for (const [_, [key, value]] of this._map) {
       yield new KopiTuple([key, await value]);
     }
   }
 
+  size() {
+    return new KopiNumber(this._map.size);
+  }
+
   async at(key: KopiValue) {
-    const value = this.map.get(await key.inspect() as string);
+    const value = this._map.get(await key.inspect() as string);
 
     if (value) {
       return value[1];
@@ -74,7 +128,7 @@ class KopiDict extends KopiValue implements AsyncIterable<KopiValue> {
   }
 
   async merge(that: KopiDict) {
-    return new KopiDict([...this.map, ...that.map]);
+    return new KopiDict([...this._map, ...that._map]);
   }
 
   async '<<'(that: KopiDict) {
