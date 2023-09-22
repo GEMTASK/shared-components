afda662797d53af71e271a912fcce2fa67c465b0
Mike Austin <mike_ekim@yahoo.com>
2023-09-22 08:48:47 -0700

    Add Editor syntax support

--- test/src/components/editor/Editor.tsx
+++ test/src/components/editor/Editor.tsx
@@ -1,7 +1,40 @@
 import React, { useEffect, useState } from 'react';
 import CodeMirror from '@uiw/react-codemirror';
+import { LRLanguage, LanguageSupport, indentNodeProp, foldNodeProp, foldInside, delimitedIndent } from "@codemirror/language";
+import { styleTags, tags as t } from "@lezer/highlight";
+
+import { parser } from './kopi-parser';
 
 import { View } from 'bare';
+
+export const KopiLanguage = LRLanguage.define({
+  parser: parser.configure({
+    props: [
+      indentNodeProp.add({
+        Application: delimitedIndent({ closing: ")", align: false })
+      }),
+      foldNodeProp.add({
+        Application: foldInside
+      }),
+      styleTags({
+        Identifier: t.variableName,
+        Number: t.number,
+        Boolean: t.bool,
+        String: t.string,
+        LineComment: t.lineComment,
+        "[ ]": t.bracket,
+        "{ }": t.brace,
+        "( )": t.paren
+      })
+    ]
+  }),
+  languageData: {
+    closeBrackets: { brackets: ["[", "{", '"'] },
+    commentTokens: { line: "#" }
+  }
+});
+
+const kopiLanguage = new LanguageSupport(KopiLanguage);
 
 const Editor = ({ args, ...props }: any) => {
   const [text, setText] = useState(args);
@@ -22,6 +55,7 @@ const Editor = ({ args, ...props }: any) => {
         value={text}
         height="100%"
         style={{ flex: 1, overflow: 'auto' }}
+        extensions={[kopiLanguage]}
       // onChange={onChange}
       />
     </View>
