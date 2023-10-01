f6fbd6a8de111e1b48e3fd0cc48a99b8cc22b374
Mike Austin <mike_ekim@yahoo.com>
2023-09-30 18:17:51 -0700

    Update Kopi example

--- react-desktop/public/webdav/scripts/vector.js
+++ react-desktop/public/webdav/scripts/vector.js
@@ -50,6 +50,33 @@ Object.defineProperty(KopiVector, 'name', {
   value: 'Vector'
 });
 
+KopiVector.applyGravity2D = (tuple) => {
+  const positions = tuple.fields[0]._elements;
+  const velocities = tuple.fields[1]._elements;
+
+  const length = Math.min(positions.length, velocities.length);
+
+  for (let i = 0; i < length; i += 2) {
+    for (let j = i + 2; j < length; j += 2) {
+      const force = 1 / Math.sqrt(
+        Math.abs(positions[i + 0] - positions[j + 0])
+        * Math.abs(positions[i + 1] - positions[j + 1])
+      ) ** 2;
+
+      velocities[i + 0] += force;
+      velocities[i + 1] += force;
+
+      velocities[j + 0] -= force;
+      velocities[j + 1] -= force;
+    }
+
+    positions[i + 0] += velocities[i + 0];
+    positions[i + 1] += velocities[i + 1];
+  }
+
+  return new KopiTuple([new KopiVector(positions), new KopiVector(velocities)]);
+};
+
 export {
   KopiVector as Vector,
 };
